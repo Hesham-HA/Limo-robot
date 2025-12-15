4e8f8f7b0fc12312e098c35e17a697914e196d14
Hesham-HA <hassanhesham980@gmail.com>
2025-12-15 12:29:46 +0200

    change map file in object search

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 # Source ROS setup
 source /opt/ros/noetic/setup.bash
 
+# Source Gazebo setup
+source /usr/share/gazebo/setup.bash
+
 # If a catkin workspace exists, source it
 if [ -f /home/ros-noetic/catkin_ws/devel/setup.bash ]; then
     source /home/ros-noetic/catkin_ws/devel/setup.bash
