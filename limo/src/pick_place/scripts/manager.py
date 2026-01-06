6e4176e2be4f8129e5314926e580a2f50f1ec686
Hesham-H98 <hassanhesham980@gmail.com>
2026-01-06 08:35:35 +0200

    updates to complete a successful mission

--- limo/src/pick_place/scripts/manager.py
+++ limo/src/pick_place/scripts/manager.py
@@ -3,6 +3,7 @@ import rospy
 from std_srvs.srv import Trigger, TriggerResponse
 from pick_place.srv import AddObjectToScene, AddObjectToSceneRequest
 import actionlib
+import dynamic_reconfigure.client
 import math
 import subprocess
 import os
@@ -12,7 +13,6 @@ from typing import Literal, Dict
 from visualization_msgs.msg import MarkerArray
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Twist, PoseStamped
-from nav_msgs.msg import Odometry
 import tf2_ros
 import tf2_geometry_msgs
 from tf.transformations import quaternion_from_euler
@@ -37,9 +37,11 @@ class PickPlaceManager:
         self.exploration = None
         self.robot_pose = None
         self.object: Dict | None = None
-        self.object_detected = False
+        self.object_detection_counts = 0 # add counts to handle detection noise
+        self.object_time_detected = 0.0
         self.table: Dict | None = None
-        self.table_detected = False
+        self.table_detection_counts = 0
+        self.table_time_detected = 0.0
         self.cubes = {}
         self.labels = {}
         
@@ -50,6 +52,9 @@ class PickPlaceManager:
         # Movebase client
         self.move_base_actions = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.move_base_actions.wait_for_server()
+        local_planner = rospy.get_param("~local_planner", 'move_base/DWAPlannerROS')
+        self.move_base_configs = dynamic_reconfigure.client.Client(local_planner, timeout=5)
+        self.local_planner_params = self.move_base_configs.get_configuration()
         
         # TF Buffer for transformations
         self.tf_buffer = tf2_ros.Buffer()
@@ -60,6 +65,8 @@ class PickPlaceManager:
         self.reset_rtabmap = rospy.ServiceProxy('/rtabmap/reset', Empty)
         rospy.wait_for_service("/rtabmap/pause")
         self.pause_rtabmap = rospy.ServiceProxy('/rtabmap/pause', Empty)
+        rospy.wait_for_service("/rtabmap/resume")
+        self.resume_rtabmap = rospy.ServiceProxy('/rtabmap/resume', Empty)
         rospy.wait_for_service("/clear_octomap")
         self.clear_octomap = rospy.ServiceProxy('/clear_octomap', Empty)
         rospy.wait_for_service("/add_object_to_scene")
@@ -71,7 +78,6 @@ class PickPlaceManager:
         
         # Subscribers "This intiates the callback when an object is detected"
         rospy.Subscriber("/object/detection", MarkerArray, self.object_detected_callback)
-        rospy.Subscriber("/odom", Odometry, self.pose_estimation)
         
         # Publishers: velocity commands
         self.cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -81,8 +87,6 @@ class PickPlaceManager:
     # start search
     def start_search(self):
         rospy.loginfo("Starting search!!!")
-        self.object_detected = False
-        self.table_detected = False
         if self.exploration:
             rospy.logwarn("Exploration is already running !!!")
             return
@@ -104,11 +108,12 @@ class PickPlaceManager:
         # Cancel current move-base goal
         rospy.loginfo("Cancelling current move_base_goal")
         self.move_base_actions.cancel_all_goals()
-        rospy.sleep(2.0)
+        rospy.sleep(1.0)
         rospy.loginfo("Forcing robot stop...")
-        for _ in range(10):
+        for _ in range(100):
             self.stop_robot()
             rospy.sleep(0.1)
+        self.move_base_actions.cancel_all_goals()
         rospy.logwarn("Goal cancelled and robot stopped!")
         # Pause rtabmapping
         self.pause_rtabmap()
@@ -120,8 +125,18 @@ class PickPlaceManager:
         self.cmd_vel.publish(Twist())
     
     # get current pose
-    def pose_estimation(self, msg):
-        self.robot_pose = msg
+    def get_robot_pose(self):
+        try:
+            # Wait for the transform to become available
+            trans = self.tf_buffer.lookup_transform("map", "base_link", rospy.Time.now(), rospy.Duration(2.0))
+            self.robot_pose = PoseStamped()
+            self.robot_pose.header.frame_id = "map"
+            self.robot_pose.pose.position.x = trans.transform.translation.x
+            self.robot_pose.pose.position.y = trans.transform.translation.y
+            self.robot_pose.pose.position.z = trans.transform.translation.z
+            self.robot_pose.pose.orientation = trans.transform.rotation
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            rospy.logerr("TF Error: %s", e)
     
     # Object/Table detection callback
     def object_detected_callback(self, msg):
@@ -151,19 +166,27 @@ class PickPlaceManager:
                         'z': marker.pose.position.z
                     }
                 }
-        obj_once_detected = self.object_detected
-        tab_once_detected = self.table_detected
+        obj_once_detected = self.object_detection_counts
+        tab_once_detected = self.table_detection_counts
         for id_, label in self.labels.items():
             label_type = label["text"]
             if (id_-1) in self.cubes:
                 if label_type == "redcube" or label_type.startswith("object"):
                     self.object = self.cubes[id_-1]
-                    self.object_detected = True
+                    self.object_detection_counts += 1
+                    if rospy.Time.now().to_sec() - self.object_time_detected > 3.0: # Two seconds span between consecutive detections
+                        self.object_detection_counts = 1
+                    self.object_time_detected = rospy.Time.now().to_sec()
                 elif label_type == "yellowtable" or label_type.startswith("table"):
                     self.table = self.cubes[id_-1]
-                    self.table_detected = True
-        if self.object_detected != obj_once_detected or self.table_detected != tab_once_detected:
-            rospy.loginfo(f"Detected object: {self.object_detected}, detected table: {self.table_detected}")
+                    self.table_detection_counts += 1
+                    if rospy.Time.now().to_sec() - self.table_time_detected > 3.0:  # Two seconds span between consecutive detections
+                        self.table_detection_counts = 1
+                    self.table_time_detected = rospy.Time.now().to_sec()
+        if self.object_detection_counts and not obj_once_detected:
+            rospy.loginfo(f"Object detected: {self.object}")
+        if self.table_detection_counts and not tab_once_detected:
+            rospy.loginfo(f"Table detected: {self.table}")
     
     # Add the detected object/table to the planning scene
     def add_to_scene(self, type: Literal["object", "table"]):
@@ -199,27 +222,41 @@ class PickPlaceManager:
         target_pose.header.stamp = rospy.Time.now()
         target_pose.pose.position.x = obj['position']["x"]
         target_pose.pose.position.y = obj['position']["y"]
-        target_pose.pose.orientation.w = 1.0  # Identity orientation for the point
+        target_pose.pose.position.z = obj['position']["z"]
+        target_pose.pose.orientation.x = obj['orientation']["x"]
+        target_pose.pose.orientation.y = obj['orientation']["y"]
+        target_pose.pose.orientation.z = obj['orientation']["z"]
+        target_pose.pose.orientation.w = obj['orientation']["w"]
+        self.get_robot_pose()
+        if self.robot_pose is None:
+            rospy.logerr("Failed to get current robot pose!")
+            return False
         try:
-            transform = self.tf_buffer.lookup_transform("map", obj["frame_id"], rospy.Time(0), rospy.Duration(1.0))
+            transform = self.tf_buffer.lookup_transform("map", obj["frame_id"], rospy.Time.now(), rospy.Duration(1.0))
             target_pose_map = tf2_geometry_msgs.do_transform_pose(target_pose, transform)
-            transform = self.tf_buffer.lookup_transform("map", self.robot_pose.header.frame_id, rospy.Time(0), rospy.Duration(1.0))
-            robot_pose_map = tf2_geometry_msgs.do_transform_pose(self.robot_pose.pose, transform)
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
             rospy.logerr(f"Failed to transform target pose: {e}")
             return False
         
         target_x = target_pose_map.pose.position.x
         target_y = target_pose_map.pose.position.y
-        robot_x = robot_pose_map.pose.position.x
-        robot_y = robot_pose_map.pose.position.y
+        robot_x = self.robot_pose.pose.position.x
+        robot_y = self.robot_pose.pose.position.y
         dx = target_x - robot_x
         dy = target_y - robot_y
         d = math.hypot(dx, dy)
         heading = math.atan2(dy, dx)
         q1, q2, q3, q4 = quaternion_from_euler(0, 0, heading)
-        if move_base:
-            factor = 0.32 / d
+        
+        if move_base: # reach to an intermediate pose
+            # Set very small goal tolerance
+            params = {"xy_goal_tolerance": 0.04, "yaw_goal_tolerance": 0.01}
+            try:
+                self.move_base_configs.update_configuration(params)
+                rospy.loginfo("Updated planner tolerances: xy=0.04 m, yaw=0.01 rad")
+            except Exception as e:
+                rospy.logwarn(f"Failed to update planner configuration: {e}")
+            factor = (d - 0.6) / d
             target_x = robot_x + dx * factor
             target_y = robot_y + dy * factor
             goal = MoveBaseGoal()
@@ -233,27 +270,33 @@ class PickPlaceManager:
             goal.target_pose.pose.orientation.z = q3
             goal.target_pose.pose.orientation.w = q4
             self.move_base_actions.send_goal(goal)
-            rospy.loginfo("Navigation goal published (offset 32 cm) in odom frame")
+            rospy.loginfo("Navigation goal published (offset 60 cm)")
             finished = self.move_base_actions.wait_for_result(rospy.Duration(60.0))
             if not finished:
                 rospy.logwarn("Timed out achieving goal, canceling.")
                 self.move_base_actions.cancel_goal()
                 return False
+            # Restore default goal tolerance
+            params = {"xy_goal_tolerance": self.local_planner_params["xy_goal_tolerance"], "yaw_goal_tolerance": self.local_planner_params["yaw_goal_tolerance"]}
+            try:
+                self.move_base_configs.update_configuration(params)
+                rospy.loginfo("Original planner tolerances are restored")
+            except Exception as e:
+                rospy.logwarn(f"Failed to restore planner configuration: {e}")
             state = self.move_base_actions.get_state()
             rospy.loginfo("Goal finished with state: %d", state)
             return True
-
-        else:
+        else: # approach to object in a straight line (approximately 32 cm distant)
             rospy.loginfo("Sending velocity commands directly towards target")
             d_traveled = 0
+            cmd = Twist()
+            cmd.linear.x = 0.02
+            cmd.angular.z = 0.0
+            self.cmd_vel.publish(cmd)
             while not rospy.is_shutdown():
-                cmd = Twist()
-                cmd.linear.x = 0.02
-                cmd.angular.z = 0.0
-                self.cmd_vel.publish(cmd)
-                rospy.sleep(20)
-                d_traveled += 1.1*0.02*20
-                if d_traveled - d > 0.05:
+                rospy.sleep(1)
+                d_traveled += 1.1*0.03*1
+                if d_traveled - d + 1.1*0.02*1 >= 0.06:
                     self.stop_robot()
                     rospy.loginfo("Reached target (cmd_vel).")
                     return True
@@ -266,12 +309,32 @@ class PickPlaceManager:
         result = self.pick_lift_object()
         if result.success:
             rospy.loginfo("Successfully picked up the object!")
+            d_traveled = 0
+            cmd = Twist()
+            cmd.linear.x = -0.05
+            cmd.angular.z = 0.0
+            self.cmd_vel.publish(cmd)
+            while not rospy.is_shutdown():
+                rospy.sleep(1)
+                d_traveled += 0.9*0.03*1
+                if d_traveled > 0.6:
+                    self.stop_robot()
+                    rospy.loginfo("Stepped backwards (cmd_vel).")
+                    return True
+            rospy.logwarn("Failed to step backwards (cmd_vel)!")
             return True
+        rospy.logerr("Failed to pick object!")
         return False
     
     def reset_mapping(self):
         rospy.loginfo("Reset RTAB-Map...")
         try:
+            rospy.set_param("/rtabmap/rtabmap/Grid/Sensor", 0)
+            rospy.set_param("/rtabmap/rtabmap/Grid/RangeMax", 3.0)
+            rospy.wait_for_service("/rtabmap/update_parameters")
+            update_params = rospy.ServiceProxy("/rtabmap/update_parameters", Empty)
+            update_params()
+            self.resume_rtabmap()
             self.reset_rtabmap()
             return True
         except rospy.ServiceException as e:
@@ -297,12 +360,16 @@ class PickPlaceManager:
             rospy.logwarn("Mission is already active. Please wait till current mission finishes!")
             response.message = "Mission is already active. Please wait till current mission finishes!"
             return response
+        self.object_detection_counts = 0
+        self.table_detection_counts = 0
+        rospy.loginfo("Starting full pick and place mission...")
         # Step 1: initiate search
         self.start_search()
         # Wait till object is detected and tracked
-        while not self.object_detected:
+        while self.object_detection_counts < 3:
             rospy.loginfo("Looking for the object .....")
             rospy.sleep(0.5)
+        rospy.loginfo("Object is found!")
         # Step 2: stop searching
         self.stop_search()
         # Step 3: add object to scene
@@ -325,9 +392,10 @@ class PickPlaceManager:
             return response
         self.start_search()
         # Wait till table is detected and tracked
-        while not self.table_detected:
+        while self.table_detection_counts < 3:
             rospy.loginfo("Looking for the placing table .....")
             rospy.sleep(0.5)
+        rospy.loginfo("Table is found!")
         # Step 8: stop searching
         self.stop_search()
         # Step 9: add table to scene
